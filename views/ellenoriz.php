de7660b727641e81821f606f9da6ca32c2fb37c0
enKeri <nagy.g3rg0@gmail.com>
2025-11-23 13:36:23 +0100

    Update ellenoriz.php
    Megcsináltam, hogy aki nem admin, ne tudjon URL ismeretével bemenni oda ahova nem kéne

--- views/ellenoriz.php
+++ views/ellenoriz.php
@@ -1,8 +1,30 @@
 <?php
-session_start();
-//$_SESSION['nev'] és $_SESSION['dolg_id'] van használva, de együtt, így bármelyik vizsgálata is elég, ezért nem lett módosítva a példa
-if (!isset($_SESSION['cahol_nev'])) {
-    header("Location: bejelentkezes.php");
-    exit();
-}
+    session_start();
+    //$_SESSION['nev'] és $_SESSION['dolg_id'] van használva, de együtt, így bármelyik vizsgálata is elég, ezért nem lett módosítva a példa
+    if (!isset($_SESSION['cahol_nev'])) {
+        header("Location: bejelentkezes.php");
+        exit();
+    }
+
+    $isAdmin = false;
+    $current_page = basename($_SERVER['SCRIPT_NAME']);
+
+    $admin_pages = 
+    [
+        'uj_auto_tipus.php',
+        'uj_auto.php',
+        'uj_osztaly.php',
+        'uj_telephely.php',
+    ];
+
+    if($_SESSION['cahol_jogosultsag'] == "admin" || $_SESSION['cahol_jogosultsag'] == "suser")
+    {
+        $isAdmin = true;
+    }
+
+    if (in_array($current_page, $admin_pages, true) && !$isAdmin) 
+    {
+        header("Location: autok_lista.php");
+        exit();
+    }
 ?>
